52b88b0f05c20aee4feb8693c00803e2533409ab
M Huzaifa <smhuzaifapk@gmail.com>
2026-05-30 00:21:50 +0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -9,59 +9,53 @@
 #else
 #define PATH_LIST_SEPARATOR ":"
 #endif
-
-void check_x(char *file_path,char *token2){
+char *path_copy;
+char file[256];
+char cmd_copy[256];
+void full_path(char *file_path, char *command){
+  
+  sprintf(file,"%s/%s" ,file_path,command);
+  //printf("PATH: %s is %s\n",command,file);
+  return;
+}
+void check_x(char *command, char *token2){
   int flag = 0;
+  char *ptr2;
+  char *file_path = strtok_r(path_copy, PATH_LIST_SEPARATOR, &ptr2);
   while(file_path != NULL){
         //printf("PATH: %s\n",file_path);
-        char file[256];
-        sprintf(file,"%s/%s" ,file_path,token2);
+        //sprintf(file,"%s/%s" ,file_path,token2);
+        //printf("%s", command);
         //printf("PATH: %s is %s\n",token2,file);
+        if(strcmp(command, "type") == 0){
+          full_path(file_path, token2);
+        }
+        else{
+          full_path(file_path,command);
+        }
+        //printf("%d\n%d\n",access(file,F_OK),access(file,X_OK));
         if (access(file,F_OK) == 0){
           if (access(file,X_OK) == 0){
-            printf("%s is %s\n",token2,file);
-            flag = 1; 
-            break;
+            flag = 1;
+            printf("TEST");
+            if(strcmp(command, "type") == 0){
+              printf("%s is %s\n",token2,file);
+              break;
+            }
+            else{
+              //printf("TEST\n");
+              //execvp(file,args);
+              system(cmd_copy);
+              break;
+            }
           }
         }
-        file_path = strtok(NULL,":");
+        file_path = strtok_r(NULL,":", &ptr2);
       }
   if (!flag){printf("%s: not found\n", token2);}
   return;
 }
 
-void execute_command(char *command, char *path){
-  char *token = strtok(command, " ");
-  char *path_copy = strdup(path);
-  int executable = 0;
-
-    
-    
-
-    if (strcmp(command, "type") == 0){
-      char *token2 = strtok(NULL,"");    
-      char *file_path = strtok(path_copy, ":");
-      if (token2 != NULL && strcmp(token2,"echo") == 0 ||
-                            strcmp(token2,"exit") == 0 ||
-                            strcmp(token2,"type") == 0 ){
-              printf("%s is a shell builtin\n", token2);
-           }
-      else {
-  
-        //int flag = 0;
-        check_x(file_path, token2);  
-      }
-    }
-
-    // else if (strcmp(command))
-
-    else {
-      system(command);
-      printf("%s: command not found\n", command);
-    }
-    free(path_copy);
-    return;
-}
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -74,6 +68,7 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   char command[256]; 
   char *path = getenv("PATH");
+  
   // if (path != NULL){
   //   printf("%s", path);
   // }
@@ -83,15 +78,44 @@ int main(int argc, char *argv[]) {
     //scanf("%s", command);
     fgets(command,sizeof(command), stdin);
     command[strlen(command) - 1 ] = '\0';
-    
+    strcpy(cmd_copy,command);
+    char *ptr1;
+    char *token = strtok_r(command, " ", &ptr1);
+    path_copy = strdup(path);
+    int execute = 0;
+    //printf("ERR: %s\n", file_path);
+    // printf("ERR: %s\n", token);
     if ( strcmp(command,"exit") == 0){
       return 0;
     }
-    if (strcmp(command,"echo") == 0){
-      printf("%s\n",strtok(NULL,""));
+    else if (strcmp(command,"echo") == 0){
+      // printf("ERR: %s\n", file_path);
+      printf("%s\n",strtok_r(NULL,"",&ptr1));
     }
     //printf("%s\n", command);
-    execute_command(command, path);
+    else if (strcmp(command, "type") == 0){
+      char *token2 = strtok_r(NULL,"",  &ptr1);    
+      
+      if (token2 != NULL && strcmp(token2,"echo") == 0 ||
+                            strcmp(token2,"exit") == 0 ||
+                            strcmp(token2,"type") == 0 ){
+              printf("%s is a shell builtin\n", token2);
+           }
+      else {
+          //int flag = 0;
+        check_x(command,token2);  
+      }
+    }
+
+    else if (strcmp(command, "type") != 0){
+      char *args = strtok_r(NULL, "", &ptr1);
+      check_x(command,args);
+    }
+    else {
+      //system(command);
+      printf("%s: command not found\n", command);
+    }
+    free(path_copy);
   }
   return 0;
 }
